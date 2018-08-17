6a31ca103dc17b630ce5dd901f2c063d354c8cbe
root <root@localhost.localdomain>
2018-08-17 07:06:47 -0700

    Transcribe commit

---
+++ Transcribe/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿///Miguel Pulido - Systems Architect
+
+namespace Transcribe
+{
+    partial class Transcribe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Transcribe));
+            this.TextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.TextBox1.ForeColor = System.Drawing.Color.Lime;
+            this.TextBox1.Location = new System.Drawing.Point(12, 12);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(460, 379);
+            this.TextBox1.TabIndex = 0;
+            this.TextBox1.Text = "";
+            this.TextBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 407);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Speak Text";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(129, 407);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 43);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Start Listening";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(245, 407);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 43);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Stop Listening";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(371, 407);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 43);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Logfile";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Transcribe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(484, 462);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Transcribe";
+            this.Text = "Transcribe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox TextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Transcribe/Form1.cs
@@ -0,0 +1,133 @@
+﻿///Miguel Pulido - Systems Architect
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+using System.IO;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Threading;
+using System.IO.Ports;
+
+
+namespace Transcribe
+{
+    public partial class Transcribe : Form
+    {
+
+        //use StreamWriter class
+        //The line directly below works, second param "true" appends
+
+        StreamWriter w = new StreamWriter(@"C:\\TestFolder\Transcribe.txt",true);
+        
+               
+
+        public Transcribe()
+        {
+            InitializeComponent();
+
+        }
+        //Setup our variables for use in our textbox
+        SpeechSynthesizer sSynth = new SpeechSynthesizer();
+        PromptBuilder pBuilder = new PromptBuilder();
+        SpeechRecognitionEngine sRecognize = new SpeechRecognitionEngine();
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pBuilder.ClearContent();
+            pBuilder.AppendText(TextBox1.Text);
+            sSynth.Speak(pBuilder);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            //timer1.Start();
+
+            //setup button toggle for enabling/disabling listening
+            button2.Enabled = false;
+            button3.Enabled = true;
+            //Set Grammar to recognize words you identify below
+            Choices sList = new Choices();
+            
+            
+            
+            
+            sList.Add(new string[] {"open", "close", "help", "fire", "earthquake", "call 911"});
+            
+            //Create Grammar based on our sList definition above
+            Grammar gr = new Grammar(new GrammarBuilder(sList));
+            
+            // Setup exception handling
+            try
+            {
+                sRecognize.RequestRecognizerUpdate();
+                sRecognize.LoadGrammar(gr);
+                sRecognize.SpeechRecognized += sRecognize_SpeechRecognized;
+                //sets default microphone
+                sRecognize.SetInputToDefaultAudioDevice();
+                //sets multi-word recognition 
+                sRecognize.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch
+            {
+
+                return;
+            }
+        }
+
+        //sets up even handler - when exit is heard, exit application 
+        private void sRecognize_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            if (e.Result.Text == "exit")
+            {
+                Application.Exit(); // listens for exit then closes application
+
+            }
+            else
+            {
+                TextBox1.Text = TextBox1.Text + " " + e.Result.Text.ToString();
+            }
+            
+            // MessageBox.Show("speech recognized:" + e.Result.Text.ToString()); // opens message box
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sRecognize.RecognizeAsyncStop();
+            button2.Enabled = true;
+            button3.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+        //writer.WriteLine();
+            w.Close();
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            //Use writeline method to write the text and 
+            //in para... put your text, i have used textBox1's text 
+
+            w.WriteLine(TextBox1.Text);
+
+            //always close your stream
+
+       //     w.Close();
+        }
+
+
+
+
+        }
+    }
+
